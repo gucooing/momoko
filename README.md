d360274f5dc94b59f3ead9b8e5db84a9f0e6c85b
gucooing <gucooing@alsl.xyz>
2026-04-06 20:20:20 +0800

    新增文件处理包

--- README.md
+++ README.md
@@ -1,17 +1,2 @@
-# Kratos Admin Template
-
-## Best Practice
-Google AIP(https://google.aip.dev/general):
-1. Resource-oriented design
-2. Filtering
-3. Pagination
-4. Field masks
-5. Field behavior
-
-## Generate API files
-```shell
-# Download and update dependencies
-make init
-# Generate API files (include: pb.go, http, grpc, validate, swagger) by proto file
-make api
-```
+# Momoko
+## 基于 [Kratos Admin Template](https://github.com/go-kratos/kratos) 编写的多功能游戏服务器管理面板
---
+++ pkg/file/file_test.go
@@ -0,0 +1,202 @@
+package file
+
+import (
+	"context"
+	"errors"
+	"io"
+	stdhttp "net/http"
+	"os"
+	"path/filepath"
+	"strconv"
+	"testing"
+
+	"github.com/go-kratos/kratos/v2/transport/http"
+)
+
+func Test_ResolveRealPath(t *testing.T) {
+	basePath := t.TempDir()
+	f, err := NewFileOper(basePath)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if _, err = f.ResolveRealPath(filepath.Dir(basePath)); err == nil {
+		t.Errorf("ResolveRealPath 失效")
+		return
+	}
+}
+
+func Test_ListDir(t *testing.T) {
+	f, err := NewFileOper("")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if err = f.ListDir("", SortByModTime, SortAsc); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func Test_Download(t *testing.T) {
+	f, err := NewFileOper("")
+	if err != nil {
+		t.Fatal(err)
+	}
+	srv := http.NewServer()
+	testCtx := context.Background()
+	router := srv.Route("/")
+	router.GET("/d", func(ctx http.Context) error {
+		path, err := f.ResolveRealPath(ctx.Query().Get("path"))
+		if err != nil {
+			return err
+		}
+		f, err := os.Open(path)
+		if err != nil {
+			return err
+		}
+		defer f.Close()
+
+		info, err := f.Stat()
+		if err != nil {
+			return err
+		}
+
+		w := ctx.Response()
+
+		w.Header().Set("Content-Type", "application/octet-stream")
+		w.Header().Set("Content-Disposition", `attachment; filename="`+info.Name()+`"`)
+		w.Header().Set("Content-Length", strconv.FormatInt(info.Size(), 10))
+
+		_, err = io.Copy(w, f)
+		if err != nil {
+			return err
+		}
+		srv.Stop(testCtx)
+		return nil
+	})
+
+	if err := srv.Start(testCtx); err != nil {
+		return
+	}
+	t.Log("Download End")
+}
+
+func Test_Download_Speed(t *testing.T) {
+	srv := http.NewServer()
+	testCtx := context.Background()
+	router := srv.Route("/")
+	router.GET("/d/speed", func(ctx http.Context) error {
+		f, err := os.Open("big.test")
+		if err != nil {
+			if os.IsNotExist(err) {
+				f, err = os.Create("big.test")
+				if err != nil {
+					return err
+				}
+				if err = f.Truncate(10 * 1024 * 1024 * 1024); err != nil {
+					f.Close()
+					return err
+				}
+				f.Close()
+
+				f, err = os.Open("big.test")
+				if err != nil {
+					return err
+				}
+			} else {
+				return err
+			}
+		}
+		defer f.Close()
+
+		info, err := f.Stat()
+		if err != nil {
+			return err
+		}
+
+		w := ctx.Response()
+		r := ctx.Request()
+
+		w.Header().Set("Content-Disposition", `attachment; filename="`+info.Name()+`"`)
+
+		stdhttp.ServeContent(w, r, info.Name(), info.ModTime(), f)
+		return nil
+	})
+
+	go func() {
+		if err := srv.Start(testCtx); err != nil {
+			return
+		}
+	}()
+	select {
+	case <-testCtx.Done():
+		srv.Close()
+	}
+	t.Log("Download_Speed End")
+}
+
+func Test_SaveFile(t *testing.T) {
+	f, err := NewFileOper("")
+	if err != nil {
+		t.Fatal(err)
+	}
+	srv := http.NewServer()
+	testCtx := context.Background()
+	router := srv.Route("/")
+	router.POST("/put", func(ctx http.Context) error {
+		path, err := f.ResolveRealPath("./")
+		if err != nil {
+			return err
+		}
+		r := ctx.Request()
+		w := ctx.Response()
+
+		mr, err := r.MultipartReader()
+		if err != nil {
+			return err
+		}
+		for {
+			part, err := mr.NextPart()
+			if err == io.EOF {
+				break
+			}
+			if err != nil {
+				return err
+			}
+			dstPath := filepath.Join(path, part.FileName())
+			dst, err := os.Create(dstPath)
+			if err != nil {
+				return err
+			}
+			_, copyErr := io.Copy(dst, part)
+			closeErr1 := dst.Close()
+			closeErr2 := part.Close()
+
+			if copyErr != nil {
+				_ = os.Remove(dstPath)
+				return copyErr
+			}
+			if closeErr1 != nil {
+				_ = os.Remove(dstPath)
+				return closeErr1
+			}
+			if closeErr2 != nil {
+				_ = os.Remove(dstPath)
+				return closeErr2
+			}
+
+			w.WriteHeader(200)
+			return nil
+		}
+
+		return errors.New("没有找到上传文件")
+	})
+	go func() {
+		if err := srv.Start(testCtx); err != nil {
+			return
+		}
+	}()
+	select {
+	case <-testCtx.Done():
+		srv.Close()
+	}
+	t.Log("SaveFile End")
+}
